5a6a0651a45506473241e59ba6425b753a25c569
lucia raschetti <lucia.sn011@gmail.com>
2020-09-24 01:33:24 -0300

    C4R: Add form validation

--- src/components/screens/Contact/ContactForm.js
+++ src/components/screens/Contact/ContactForm.js
@@ -9,16 +9,47 @@ const onSubmit = () => (
 const ContactForm = () => (
     <Form
       onSubmit={onSubmit}
-      render={({ handleSubmit }) => (
+      validate={values => {
+          const errors = {}
+          if(!values.name) {
+              errors.name = 'Please enter 3 characters or more';
+              errors.email = 'Please enter a valid email';
+              errors.message = 'Please enter 5 characters or more';
+          }
+          return errors;
+      }}
+      render={({ handleSubmit, values }) => (
         <form onSubmit={handleSubmit}>
             <h2>GET IN TOUCH</h2>
             <label htmlFor="name">Name</label>
-            <Field id="name" className="entry" component="input" type="text" name="name"/>
+            <Field id="name" component="input" type="text" name="name">
+                {({input, meta}) => (
+                 <div>
+                     <input className="entry" {...input} />
+                     {meta.touched && meta.error && <p>{meta.error}</p>}
+                 </div>       
+                )}
+            </Field>
             <label htmlFor="email">Email</label>
-            <Field id="email" className="entry" component="input" type="text" name="email"/>
+            <Field id="email" component="input" type="text" name="email">
+                {({input, meta}) => (
+                 <div>
+                     <input className="entry" {...input} />
+                     {meta.touched && meta.error && <p>{meta.error}</p>}
+                 </div>       
+                )}
+            </Field>
             <label htmlFor="message">Message</label>
-            <Field id="message" className="entry" component="textarea" name="message"></Field>
+            <Field id="message" component="textarea" name="message">
+                {({input, meta}) => (
+                    <div>
+                        <textarea {...input} />
+                        {meta.touched && meta.error && <p>{meta.error}</p>}
+                    </div>       
+                    )}
+            </Field>
             <button id="submit" type="submit">Send Message</button>
+            <pre>{JSON.stringify(values, undefined, 2)}</pre>
         </form>
       )}
     />
