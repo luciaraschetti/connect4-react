5a208f2349905c52e49b86f59d416e013c668c69
lucia raschetti <lucia.sn011@gmail.com>
2020-09-24 00:59:43 -0300

    C4R: Switch to react final form

--- src/components/screens/Contact/ContactForm.js
+++ src/components/screens/Contact/ContactForm.js
@@ -1,16 +1,27 @@
 import './ContactForm.css';
 import React from 'react';
+import { Form, Field } from 'react-final-form';
+
+const onSubmit = () => (
+    console.log('ok')
+)
 
 const ContactForm = () => (
-    <form>
-        <h2>GET IN TOUCH</h2>
-        <label htmlFor="name">Name</label>
-        <input id="name" className="entry" type="text" name="name"/>
-        <label htmlFor="email">Email</label>
-        <input id="email" className="entry" type="text" name="email"/>
-        <label htmlFor="message">Message</label>
-        <textarea id="message" className="entry" name="message"></textarea>
-        <button id="submit" type="button">Send Message</button>
-    </form>
+    <Form
+      onSubmit={onSubmit}
+      render={({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+            <h2>GET IN TOUCH</h2>
+            <label htmlFor="name">Name</label>
+            <Field id="name" className="entry" component="input" type="text" name="name"/>
+            <label htmlFor="email">Email</label>
+            <Field id="email" className="entry" component="input" type="text" name="email"/>
+            <label htmlFor="message">Message</label>
+            <Field id="message" className="entry" component="textarea" name="message"></Field>
+            <button id="submit" type="submit">Send Message</button>
+        </form>
+      )}
+    />
 )
+
 export default ContactForm;
